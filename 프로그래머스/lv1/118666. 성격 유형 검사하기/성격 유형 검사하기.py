59bbddb1015f5cc0a3b8dc6419893d5f13f1c1aa
Minji <76423946+my970524@users.noreply.github.com>
2022-09-10 17:02:45 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.20 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -0,0 +1,39 @@
+def solution(survey, choices):
+    # 각 지표에 대한 점수 초기화
+    score = {'R': 0, 'T':0, 'C':0, 'F':0, 'J':0, 'M':0, 'A':0, 'N':0}
+    answer = ''
+    
+    for i in range(len(choices)):
+        # choices에서 비동의에 해당하는 범위라면,
+        if 1 <= choices[i] <= 3:
+            # survey[i][0] 캐릭터가 비동의 선택 유형이므로 
+            # score[survey[i][0]] 의 점수를 더해줌
+            score[survey[i][0]] += 4 - choices[i]
+        
+        # choices에서 동의에 해당하는 범위라면,
+        elif 5 <= choices[i] <= 7:
+            # survey[i][0] 캐릭터가 동의 선택 유형이므로
+            # score[survey[i][1]] 의 점수를 더해줌
+            score[survey[i][1]] += choices[i] - 4
+        else:
+            # choices에서 모르겠음을 선택했으므로, 점수 변화 없음
+            pass
+    # 각 항목에 대해 값 비교해서 answer에 추가
+    if score['R'] == score['T']:
+        answer += 'R'
+    else:
+        answer += 'R' if score['R'] > score['T'] else 'T'
+    if score['C'] == score['F']:
+        answer += 'C'
+    else:
+        answer += 'C' if score['C'] > score['F'] else 'F'
+    if score['J'] == score['M']:
+        answer += 'J'
+    else:
+        answer += 'J' if score['J'] > score['M'] else 'M'
+    if score['A'] == score['N']:
+        answer += 'A'
+    else:
+        answer += 'A' if score['A'] > score['N'] else 'N'
+
+    return answer
