b0b356c18690481fd9f1581650593d90041e6d94
Minji <76423946+my970524@users.noreply.github.com>
2022-09-11 19:56:19 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,33 +1,22 @@
 def solution(id_list, report, k):
-#     report = list(set(report))
-#     get_report = []
-#     for i in report:
-#         get_report.append(i.split(' ')[1])
-
-#     suspended = []
-#     for i in set(get_report):
-#         if get_report.count(i) >= k:
-#             suspended.append(i)
-
-#     answer = [0 for _ in range(len(id_list))]
-#     for i in range(len(id_list)):
-#         for j in suspended:
-#             if f'{id_list[i]} {j}' in report:
-#                 answer[i] += 1
+    # 중복 신고 제거
     report = set(report)
-    answer = [0 for _ in range(len(id_list))]
-    get_report = {}
+    # 신고받은 사람의 횟수를 기록할 딕셔너리 get_reported 생성
+    # id_list에 담긴 유저별로 신고받은 횟수를 0으로 초기화
+    get_reported = {id:0 for id in id_list}
+    # 각 유저별로 메일을 몇 개 받을지 기록할 딕셔너리 get_mail 생성
+    # id_list에 담긴 유저별로 메일 받을 횟수를 0으로 초기화
+    get_mail = {id:0 for id in id_list}
     
-    for i in id_list:
-        get_report[i] = 0
-
-    for i in report:
-        a, b = i.split()
-        get_report[b] += 1
-
-    for i in report:
-        a, b = i.split()
-        if get_report[b] >= k:
-            answer[id_list.index(a)] += 1
-
-    return answer
+    # ' '를 기준으로 r을 쪼갰을 때, 1번 인덱스의 값이 신고받은 사람
+    for r in report:
+        get_reported[r.split(' ')[1]] += 1
+    
+    # report의 요소를 돌면서 신고받은 사람이 k번 이상 신고받았으면, 
+    # 그 사람을 신고한 사람은 메일을 한 통 받는다.
+    for r in report:
+        if get_reported[r.split(' ')[1]] >= k:
+            get_mail[r.split(' ')[0]] += 1
+    
+    # get_mail에서 value값만 list로 return 한다.
+    return list(get_mail.values())
