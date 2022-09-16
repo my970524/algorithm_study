4fdfb834ccb139ff3bb2a0e301a40fffa66238f0
Minji <76423946+my970524@users.noreply.github.com>
2022-09-16 11:11:48 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -6,11 +6,3 @@ def solution(numbers):
         if i not in numbers:
             answer += i
     return answer
-
-# 다른 사람의 풀이
-def solution(numbers):
-    '''
-    numbers에는 0~9까지의 숫자 중 일부가 빠져있고, 빠진 숫자들의 합을 구하는 문제
-    => 0~9까지의 합 - numbers에 있는 숫자들의 합
-    '''
-    return 45 - sum(numbers)
