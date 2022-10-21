adb46cabe5978027ae11e299bf8a1a581bb20cae
Minji <76423946+my970524@users.noreply.github.com>
2022-10-21 15:20:27 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -4,15 +4,13 @@ def get_yellow_cases(num):
     for i in range(1, m+1):
         if num % i == 0:
             result.append((i, num//i))
-            if i != num // i:
-                result.append((i, i))
     return result
 
 def solution(brown, yellow):
     answer = []
     brown_without_corner = brown - 4
     yellow_cases = get_yellow_cases(yellow)
-
+    print(yellow_cases)
     for case in yellow_cases:
         if (case[0] + case[1]) * 2 == brown_without_corner:
             answer.append(case[0] + 2)
