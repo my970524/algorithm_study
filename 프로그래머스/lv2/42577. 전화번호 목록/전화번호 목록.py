114eae80014efcf895be1f1cb8218a1a0c960f28
Minji <76423946+my970524@users.noreply.github.com>
2022-11-01 22:25:47 +0900

    [level 2] Title: 전화번호 목록, Time: 226.63 ms, Memory: 37.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,7 @@
+def solution(phone_book):
+    phone_book.sort(key=lambda x: (x, len(x)))
+    n = len(phone_book)
+    for i in range(n-1):
+        if phone_book[i+1].startswith(phone_book[i]):
+            return False
+    return True
