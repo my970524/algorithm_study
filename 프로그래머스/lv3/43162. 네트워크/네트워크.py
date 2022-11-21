5475a51390453904677f99520cb89e799545ce8a
Minji <76423946+my970524@users.noreply.github.com>
2022-11-21 18:20:15 +0900

    [level 3] Title: 네트워크, Time: 0.34 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+def bfs(graph, v, visited):
+    result = []
+    queue = deque()
+    queue.append(v)
+    while queue:
+        v = queue.popleft()
+        if visited[v-1] == False:
+            visited[v-1] = True
+            result.append(v)
+        for i in graph[v]:
+            if visited[i-1] == False:
+                queue.append(i)
+    return visited
+        
+    
+
+def solution(n, computers):
+    answer = 0
+    visited = [False] * n
+    graph = [[] for _ in range(n+1)]
+    for i in range(n):
+        for j in range(n):
+            if i != j and computers[i][j] == 1:
+                graph[i+1].append(j+1)
+
+    true_num = 0
+    for i in range(1, n+1):
+        bfs_result = bfs(graph, i, visited)
+        if bfs_result.count(True) > true_num:
+            true_num = bfs_result.count(True)
+            answer += 1
+
+    return answer
