d4dd47188a86e1c74dffb9e526cbabc2b55d5621
Minji <76423946+my970524@users.noreply.github.com>
2023-08-05 14:43:59 +0900

    [Silver III] Title: 달팽이, Time: 1232 ms, Memory: 125272 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.py
+++ 백준/Silver/1913. 달팽이/달팽이.py
@@ -1,60 +1,54 @@
-from datetime import datetime
-def sol():
-    n = int(input())
-    target = int(input())
+n = int(input())
+target = int(input())
 
-    mat = [[0 for _ in range(n)] for _ in range(n)]
+mat = [[0 for _ in range(n)] for _ in range(n)]
 
-    start_num = n ** 2
+start_num = n ** 2
 
-    if target == start_num:
-        target_position = [0, 0]
-    else:
-        target_position = []
+if target == start_num:
+    target_position = [0, 0]
+else:
+    target_position = []
 
-    directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
-    cur_direction = directions[0]
+directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+cur_direction = directions[0]
 
-    position = [0, 0]
-    mat[0][0] = start_num
+position = [0, 0]
+mat[0][0] = start_num
 
-    for i in range(start_num, 1, -1):
-        possible_direction = []
-        for j in range(len(directions)):
-            next_position = [x+y for x,y in zip(position, directions[j])]
+for i in range(start_num, 1, -1):
+    possible_direction = []
+    for j in range(len(directions)):
+        next_position = [x+y for x,y in zip(position, directions[j])]
 
-            a, b = next_position
-            if a < 0 or a >= n or b < 0 or b >= n:
+        a, b = next_position
+        if a < 0 or a >= n or b < 0 or b >= n:
+            pass
+        else:
+            next_position_value = mat[a][b]
+
+            if next_position_value > 0:
                 pass
             else:
-                next_position_value = mat[a][b]
+                possible_direction.append(directions[j])
 
-                if next_position_value > 0:
-                    pass
-                else:
-                    possible_direction.append(directions[j])
+    if len(possible_direction) > 1:
+        cur_direction = cur_direction
+    else:
+        cur_direction = possible_direction[0]
 
-        if len(possible_direction) > 1:
-            cur_direction = cur_direction
-        else:
-            cur_direction = possible_direction[0]
+    position = [x+y for x,y in zip(position, cur_direction)]
 
-        position = [x+y for x,y in zip(position, cur_direction)]
+    x, y = position
 
-        x, y = position
+    mat[x][y] = i -1
 
-        mat[x][y] = i -1
+    if mat[x][y] == target:
+        target_position = [x, y]
 
-        if mat[x][y] == target:
-            target_position = [x, y]
+for i in range(len(mat)):
+    values = (' ').join(map(str, mat[i]))
+    print(values)
 
-    for i in range(len(mat)):
-        values = (' ').join(map(str, mat[i]))
-        print(values)
-
-    x, y = target_position
-    print(x+1, y+1)
-
-    return 0
-
-sol()
+x, y = target_position
+print(x+1, y+1)
